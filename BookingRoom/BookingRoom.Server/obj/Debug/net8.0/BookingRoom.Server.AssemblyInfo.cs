829da7aaef494198a5ee407e42256743881a0c09
phuong1274 <phuongthai12070427@gmail.com>
2025-04-02 08:58:13 +0700

    prepare merge with kien branch

--- BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
+++ BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafb07195dda082d06be3c53e430318b23a05858")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad92053f73a13b51915a2fc1e5ba5d0a7164e64")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
