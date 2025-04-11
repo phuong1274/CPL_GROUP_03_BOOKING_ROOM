643115593f2f8ccaed7c5b4b1fad5a9020a53396
phuong1274 <phuongthai12070427@gmail.com>
2025-04-11 19:56:10 +0700

    nothing

--- BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
+++ BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a89ea1a3858e3508ac37be41e819a3e2fc4578a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12be50d1341bc6a337c2ebe2a373c015ae3cd09a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
