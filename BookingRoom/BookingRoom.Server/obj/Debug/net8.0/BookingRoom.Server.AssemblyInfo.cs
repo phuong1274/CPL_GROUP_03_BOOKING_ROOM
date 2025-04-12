b88f1cb88fad7d4a5bb631c5b975c883d05a47ab
kienHD03 <kienhoang131003@gmail.com>
2025-04-12 18:21:45 +0700

    Update login and register

--- BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
+++ BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12be50d1341bc6a337c2ebe2a373c015ae3cd09a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd15e4fa69c3abedbdf4c705f77545190d7e0bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
