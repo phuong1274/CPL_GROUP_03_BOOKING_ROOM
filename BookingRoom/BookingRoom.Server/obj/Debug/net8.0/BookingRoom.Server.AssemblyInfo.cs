969da69802a19dc4140b97b84c518cda9118eb8c
phuong1274 <phuongthai12070427@gmail.com>
2025-04-13 11:54:17 +0700

    fix confic

--- BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
+++ BookingRoom/BookingRoom.Server/obj/Debug/net8.0/BookingRoom.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7f374c71ba5bf1cca47a1c515388a4b7db91d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8475ff2f039ee2900a3206491124d3e26f8f145")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingRoom.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
